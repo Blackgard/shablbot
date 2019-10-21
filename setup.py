e4340c22ee61f271bb20c803f274de5f82376ceb
Blackgard <sasna142@gmail.com>
2019-10-21 17:13:01 +0700

    Add License and info about author

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shablbot',
+    version='1.0.0',
+    packages=find_packages(),
+    author='blackgard',
+    author_email='sasna142@gmail.com',
+    url='https://github.com/blackgard/shablbot',
+)
